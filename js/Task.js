e8feffeb20ca38b01aaa80ccc0975669c8bd6344
andito28 <andito763@gmail.com>
2024-02-29 13:43:24 +0800

    create featur delete

--- js/Task.js
+++ js/Task.js
@@ -4,9 +4,15 @@ class Task {
   }
 
   saveTask(taskData) {
+    //function dd-mm-yy
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
     const newTask = {
       id: Date.now(),
       isCompleted: false,
+      createAt: `${year}-${month}-${day}`,
       ...taskData,
     };
 
@@ -20,5 +26,27 @@ class Task {
 
   getTasks() {
     return JSON.parse(localStorage.getItem("tasks")) || [];
+  }
+
+  completeTask(taskId) {
+    const index = this.tasks.findIndex((task) => task.id == taskId);
+
+    if (index !== -1) {
+      this.tasks.up;
+      this.updateLocalStorage();
+    }
+  }
+
+  deleteTask(taskId) {
+    const index = this.tasks.findIndex((task) => task.id == taskId);
+
+    if (index !== -1) {
+      this.tasks.splice(index, 1);
+      this.updateLocalStorage();
+    }
+  }
+
+  updateLocalStorage() {
+    localStorage.setItem("tasks", JSON.stringify(this.tasks));
   }
 }
